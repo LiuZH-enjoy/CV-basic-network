5b309b28cf7c86e89a65254034683e2087b9666a
LiuZH-enjoy <1582037626@qq.com>
2021-09-16 19:02:25 +0800

    add comment

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -2,9 +2,16 @@ import torch.nn as nn
 import torch
 
 # 网络层
+# 网络都是继承nn.Module来新写一个类
+# 一般网络定义需要重写两个函数：__init__函数与forward()函数
+# __init__函数是初始化定义一些forward要用的网络模块，例如全连接、卷积、池化等等
+# forward函数，我的理解是操作输入数据x如何在模型中一步步流动的
+
 class AlexNet(nn.Module):
     def __init__(self, num_classes=1000, init_weights=False):
         super(AlexNet, self).__init__()
+
+        # 定义特征层，就是如何组合卷积以及最大池化来提取图形特征
         self.features = nn.Sequential(
             nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
             nn.ReLU(inplace=True),
@@ -20,6 +27,8 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
         )
+
+        # 定义分类器，就是如何把卷积后得到的特征转为全连接（其实特征层和分类器都是认为分的，不一定要分开写）
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
             nn.Linear(128 * 6 * 6, 2048),
@@ -38,12 +47,16 @@ class AlexNet(nn.Module):
         x = self.classifier(x)
         return x
 
+    # 模型权重初始化
     def _initialize_weights(self):
+        # 对模型每一层进行遍历检索
         for m in self.modules():
+            # 如果该层为卷积层，分别对weight与bias进行初始化，具体为什么要用这些初始化方法我还没搞懂，先不管
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
+            # 如果该层是全连接层，也进行相应的初始化
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
--- AlexNet/train.py
+++ AlexNet/train.py
@@ -12,8 +12,14 @@ from tqdm import tqdm
 from model import AlexNet
 
 def main():
+    # 选择device，如果可以gpu则选择gpu，否则就用cpu
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
+    # 定义一个图形增强的dict，包含train和val两个变换类型，具体怎么进行数据增强查看transforms的API，这是最好的办法
+    # RandomResizedCrop : 选定图片某一随机位置，然后按照size进行裁剪
+    # RandomHorizontalFlip : 图形以一定概率进行随机水平翻转
+    # ToTensor : Convert a PIL Image or numpy.ndarray to tensor.
+    # Normalize : 对每个channel指定means和std，每个channel分别标准化
     data_transform = {"train":transforms.Compose([transforms.RandomResizedCrop(224), 
                             transforms.RandomHorizontalFlip(),
                             transforms.ToTensor(),
@@ -23,20 +29,26 @@ def main():
                             transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])}
     data_root = os.getcwd()  # get data root path
     image_path = os.path.join(data_root, "AlexNet", "data_set", "flower_data")  # flower data set path
+    # 判断文件是否存在
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    # ImageFolder使用参考：https://www.cnblogs.com/wanghui-garcia/p/10649364.html
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
                         transform=data_transform["train"])
-    
+    # 统计所读训练集的图片数量
     train_num = len(train_dataset)
+    # 成员变量：self.class_to_idx - 类名对应的 索引
     # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
     flower_list = train_dataset.class_to_idx
     cla_dict = dict((val, key) for key, val in flower_list.items())
+    # 写入json文件
     json_str = json.dumps(cla_dict, indent=4)
     with open('./AlexNet/class_indices.json', 'w') as json_file:
         json_file.write(json_str)
+    
     batch_size = 32
     nw = min([os.cpu_count(), batch_size if batch_size>1 else 0, 8]) # number of workers
     print('Using {} dataloader workers every process'.format(nw))
+    # 将数据集变成loader，再进行训练
     train_loader = torch.utils.data.DataLoader(train_dataset, 
                             batch_size=batch_size, 
                             shuffle=True, 
@@ -49,14 +61,19 @@ def main():
                             num_workers=nw)
     print("using {} images for training, {} images for validation.".format(train_num, val_num))
 
+    # 实例化网络
     net = AlexNet(num_classes=5, init_weights=True)
+    # 牢记要转到device
     net.to(device)
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.0002)
 
-    epochs=1
+    epochs=10
+    # 模型保存路径
     save_path = './AlexNet/AlexNet.pth'
+    # 记录模型最高准确率
     best_acc = 0.0
+    # 这里的steps = 训练(验证)集数据总量 / 训练(验证)集的batch_size。也就是按照batch_size去读数据，需要读多少次
     train_steps = len(train_loader)
     validate_steps = len(validate_loader)
     for epoch in range(epochs):
@@ -64,6 +81,8 @@ def main():
         net.train()
         training_loss=0.0
         validate_loss=0.0
+        # 利用tqdm为数据读取中，提供进度条显示，有可视化与提醒作用
+        # 用法：tqdm(data_loader)
         train_bar = tqdm(train_loader)
         for step, data in enumerate(train_bar):
             images, labels = data
@@ -73,8 +92,9 @@ def main():
             train_step_loss.backward()
             optimizer.step()
 
-            # print statistics
+            # training_loss记录的是每个epoch的总loss，train_step_loss记录的是一个epoch里每个bath_size的loss
             training_loss += train_step_loss.item()
+            # desc功能，显示进度条的同时输出每个epoch里，每个batch_size的loss
             train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch+1, epochs, train_step_loss)
 
         # validate
@@ -92,9 +112,11 @@ def main():
             val_bar.desc = "validate epoch[{}/{}] loss:{:.3f}".format(epoch+1, epochs, validate_step_loss)
         val_accuracy = acc / val_num
 
+        # 每个epoch总结一次，输出train_loss和validate_loss的平均loss以及在验证机上的准确率
         print('[epoch %d] train_loss: %.3f validate_loss: %.3f val_accuracy: %.3f' % 
             (epoch+1, training_loss/train_steps, validate_loss/validate_steps, val_accuracy))
         
+        # 如果该epoch训练出来的模型在验证集上的准确率比最高的acc要高，怎保存该准确率并且保存该模型
         if val_accuracy > best_acc:
             best_acc = val_accuracy
             torch.save(net.state_dict(), save_path)
